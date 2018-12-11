c54968716cde119dfb8c12665b513349c88a864d
Shielarose <shielarosegandhi@gmail.com>
2018-12-10 20:47:07 -0500

    changed GoBackToRegistriesDashNav to a simple component

--- src/Components/GoBackToRegistriesDashNav.js
+++ src/Components/GoBackToRegistriesDashNav.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class GoBackToRegistriesDashNav extends Component {
-   render() {
-      return (
-         <div className="subNav">
-            <Link to="/registries">Go back to registries dashboard</Link>
-         </div>
-      )
-   }
+const GoBackToRegistriesDashNav = () => {
+   return (
+      <div className="subNav">
+         <Link to="/registries">Go back to registries dashboard</Link>
+      </div>
+   )
 }
 
 export default GoBackToRegistriesDashNav;
